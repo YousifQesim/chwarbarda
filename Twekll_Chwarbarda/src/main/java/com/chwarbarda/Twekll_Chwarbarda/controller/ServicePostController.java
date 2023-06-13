e1985ad9b3e346b37468cfbba9b325dcf2f6ac34
{yusifqasim} <{yusifqasim22@gmail.com>
2023-06-14 00:55:51 +0300

    Home page done

--- Twekll_Chwarbarda/src/main/java/com/chwarbarda/Twekll_Chwarbarda/controller/ServicePostController.java
+++ Twekll_Chwarbarda/src/main/java/com/chwarbarda/Twekll_Chwarbarda/controller/ServicePostController.java
@@ -3,33 +3,59 @@ package com.chwarbarda.Twekll_Chwarbarda.controller;
 import com.chwarbarda.Twekll_Chwarbarda.Repo.ServicePostRepository;
 import com.chwarbarda.Twekll_Chwarbarda.models.ServicePost;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/")
+
+@Controller
+
+
 public class ServicePostController {
     @Autowired
     private ServicePostRepository servicePostRepository;
+
 
 
     public ServicePostController(ServicePostRepository servicePostRepository) {
         this.servicePostRepository = servicePostRepository;
     }
 
-    @GetMapping("services")
+    @GetMapping("/services")
     public List<ServicePost> getAllServicePosts() {
         return servicePostRepository.findAll();
     }
-    @GetMapping("")
-    public String welcome() {
-        return "welcome sir";
+
+
+
+    @GetMapping("/")
+    public String getAllServicePosts(Model model) {
+        List<ServicePost> servicePosts = servicePostRepository.findAll();
+        model.addAttribute("servicePosts", servicePosts);
+        return "index";
     }
-    @PostMapping("addservice")
+
+
+
+    @PostMapping("/addservice")
     public ServicePost createServicePost(@RequestBody ServicePost servicePost) {
         return servicePostRepository.save(servicePost);
     }
+
+
+
+
+
+
+
+
+
+
+
 
     // Add other CRUD operations as needed
 }
